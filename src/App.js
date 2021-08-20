44647d9b2cc8311f86a389dd606f90e4163d6762
rcoder23 <rohitsrivastava2303@gamil.com>
2021-08-20 15:30:02 +0530

    added about using router and enable dark mode also for it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Abount from './components/Abount';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-let a="rohit";
 function App() {
 
   const [mode , setmode ] = useState('light');
-
+  
   const toggleMode=()=>{
     if(mode==='light'){
       setmode('dark');
@@ -17,13 +25,13 @@ function App() {
       document.getElementById("exa").style.color="white";
       document.getElementById("stt").style.color="white";
       document.getElementById("stt2").style.color="white";
-      // document.getElementsByClassName("stt").style.color="red";
+     
     }else{
       setmode('light');
       document.body.style.backgroundColor="white";
       document.getElementById("exa").style.backgroundColor="white";
       document.getElementById("exa").style.color="black";
-      // document.getElementById("stt").style.backgroundColor="red";
+      
       document.getElementById("stt").style.color="black";
       document.getElementById("stt2").style.color="black";
     }
@@ -32,15 +40,22 @@ function App() {
   return (
     
       <>
-        
+      <Router>
         <Navbar title="TextUtils" aboutText="aboutof Utils" mode={mode} toggleMode={toggleMode}></Navbar>
-        <div className="container my-3">
-
-       
-        <TextForm heading="Enter text to anaylse " mode={mode} toggleMode={toggleMode}></TextForm>
+        <Switch>
+          <Route exact path="/about">
+            <Abount></Abount>
+          </Route>
+          
+          <Route exact path="/">
+                <div className="container my-3">
+                <TextForm heading="Enter text to anaylse " mode={mode} toggleMode={toggleMode}></TextForm>
+                </div>
+                {/* <Abount></Abount> */}
+          </Route>
+        </Switch>
         
-        </div>
-        
+        </Router> 
       </>
     );
 }
--- src/components/Abount.js
+++ src/components/Abount.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Abount() {
+export default function Abount(pros) {
     return (
-        <div>
-            <h2> This is website the  handle all basic wrting skills</h2>
+        <>
+        <br/>
+        <div id="exa" className="container">
+            <h2 id="stt">This web site helps to manipulate text and performe various actions .</h2>
+            <br/>
+            <p id="stt2">By=Rohit Kumar Srivastava </p>
         </div>
+
+        </>
     )
 }
